66de79a1abb73e2afe0aec31f02d61767e1967a2
carrot93 <carrot.live@hotmail.com>
2024-10-24 09:06:44 +0800

    fix d3 model load error

--- package.js
+++ package.js
@@ -1,19 +1,22 @@
 Package.describe({
-  name: 'mariuspop:cal-heatmap',
-  version: '0.0.1',
+  name: 'shjiaye:cal-heatmap',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'A javascript module to create a calendar heatmap',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/mariushop/cal-heatmap',
+  git: 'https://github.com/carrot93/cal-heatmap',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
+Npm.depends({
+    'd3':'3.5.8'
+});
+
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   api.use('ecmascript');
-  api.use('d3js:d3@3.5.8');
   api.addFiles(['cal-heatmap.js','cal-heatmap.css'],"client");
 });
 
